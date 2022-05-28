b26f4f81578ac36b86abaa79fe52afbfa28a7fd1
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-05-28 18:10:11 -0500

    Agregar archivos de proyecto.

---
+++ WebService03Cliente/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebService03Cliente
+{
+    public partial class Form1 : Form
+    {
+
+        private ListasWs.WebServiceListas listas;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listas = new ListasWs.WebServiceListas();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] data = listas.ListaSimple();
+
+            string msg = "";
+
+            foreach(int x in data)
+            {
+                msg += x + "\r\n";
+            }
+
+            textBox1.Text = msg;
+        }
+    }
+}
